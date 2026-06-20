f37f7457e8cc53a2b40a9ebb4d7617af1c2da0ea
Jojo <jhoecabornay@gmail.com>
2026-06-20 22:06:02 +0800

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,20 @@
+async function loader() {
+  const path = "/api/home-page";
+  const BASE_URL = "http://localhost:1337";
+  const url = new URL(path, BASE_URL);
+
+  const response = await fetch(url.href);
+  const data = await response.json();
+  return { ...data.data };
+}
+
+export default async function HomeRoute() {
+  const data = await loader();
+  console.log(data);
+  return (
+    <div>
+      <h1 className="font-bold text-7xl">{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
+}
